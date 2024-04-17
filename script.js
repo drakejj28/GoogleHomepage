414588791079a60299fbb730e55c22dbcbc3d36f
Joshua Drakeford <JD@Administrators-MacBook-Pro.local>
2024-04-17 09:17:05 -0400

    pushing  repo

---
+++ script.js
@@ -0,0 +1,5 @@
+let buttonSearch = document.getElementById("submit")
+let inputVals = document.getElementById("search-bar") 
+buttonSearch.addEventListener("click", function displayE(){
+  window.alert(inputVals.value)
+})
